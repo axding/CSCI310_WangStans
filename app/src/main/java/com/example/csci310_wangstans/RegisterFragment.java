a199f9630f50b65e823e8693cb022f2ae405e06a
brennajc <brennajc@usc.edu>
2022-03-28 00:52:29 -0700

    added register page and map home screen page

--- app/src/main/java/com/example/csci310_wangstans/RegisterFragment.java
+++ app/src/main/java/com/example/csci310_wangstans/RegisterFragment.java
@@ -1,27 +1,25 @@
 package com.example.csci310_wangstans;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link RegisterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
-
+public class RegisterFragment extends Fragment {
     Button buttonLogin, buttonRegister;
-    EditText editUserName, editPassword;
+    EditText editUserName, editPassword, editEmail;
 
-    public LoginFragment() {
+    public RegisterFragment() {
         // Required empty public constructor
     }
 
@@ -30,8 +28,8 @@ public class LoginFragment extends Fragment {
      * this fragment using the provided parameters.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static RegisterFragment newInstance(String param1, String param2) {
+        RegisterFragment fragment = new RegisterFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -42,32 +40,27 @@ public class LoginFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         editUserName = view.findViewById(R.id.editUserName);
         editPassword = view.findViewById(R.id.editPassword);
+        editEmail = view.findViewById(R.id.editEmail);
 
-        buttonLogin = view.findViewById(R.id.buttonLogin);
         buttonRegister = view.findViewById(R.id.buttonRegister);
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NavHostFragment.findNavController(LoginFragment.this)
-                        .navigate(R.id.action_LoginFragment_to_MapHomePage);
-            }
-        });
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NavHostFragment.findNavController(LoginFragment.this)
-                        .navigate(R.id.action_LoginFragment_to_RegisterFragment);
+                NavHostFragment.findNavController(RegisterFragment.this)
+                        .navigate(R.id.action_RegisterFragment_to_LoginFragment);
             }
         });
+
         return view;
     }
 }
