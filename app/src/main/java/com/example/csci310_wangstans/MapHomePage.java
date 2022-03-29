86eb3b96043395e4c5f9162a819086034a0988f1
mcho9434 <mcho9434@usc.edu>
2022-03-28 21:43:25 -0700

    map home updates

--- app/src/main/java/com/example/csci310_wangstans/MapHomePage.java
+++ app/src/main/java/com/example/csci310_wangstans/MapHomePage.java
@@ -1,14 +1,23 @@
 package com.example.csci310_wangstans;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.csci310_wangstans.databinding.FragmentMapHomePageBinding;
+
+import java.util.Vector;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +27,13 @@ import android.widget.LinearLayout;
 public class MapHomePage extends Fragment {
 
     View hsc, lyon, racquetball, uac, village, track;
-    LinearLayout reservations;
+    TextView reservations;
+
+    Vector<Reservation> allUserRes = new Vector<>();
+    Vector<Reservation> comingRes = new Vector<>();
+    private FragmentMapHomePageBinding binding;
+
+    private SharedPreferences userDB;
 
     public MapHomePage() {
         // Required empty public constructor
@@ -50,9 +65,10 @@ public class MapHomePage extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map_home_page, container, false);
+        binding = FragmentMapHomePageBinding.inflate(inflater, container, false);
 
-        reservations = (LinearLayout) view.findViewById(R.id.reservation);
-
+        reservations = view.findViewById(R.id.upcomingReservations);
+        loadQuickView();
         reservations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,6 +132,85 @@ public class MapHomePage extends Fragment {
             }
         });
 
-        return view;
+        return binding.getRoot();
+
     }
+
+    private void loadQuickView() {
+        //load info
+        getUpcomingEvents();
+        loadWindow();
+    }
+
+    private void loadWindow() {
+        LinearLayout layout=binding.reservations;
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        if(comingRes.size()==0){
+            TextView noResText = new TextView(getContext());
+            noResText.setLayoutParams((layoutParams));
+            noResText.setGravity(Gravity.CENTER);
+            noResText.setText("No upcoming reservations. Try scheduling one.");
+            layout.addView(noResText);
+            return;
+        }
+        for(int i=0;i<comingRes.size();i++){
+
+            Reservation res=comingRes.get(i);
+            String start=res.getStartTime().substring(0,2)+":"+res.getStartTime().substring(2,4);
+            String end=res.getEndTime().substring(0,2)+":"+res.getEndTime().substring(2,4);
+            String time = start+ " - " + end;
+
+            TextView timeText = new TextView(getContext());
+            timeText.setLayoutParams(layoutParams);
+            timeText.setGravity(Gravity.CENTER);
+            timeText.setText(time);
+
+            TextView loc = new TextView(getContext());
+            loc.setLayoutParams((layoutParams));
+            loc.setGravity(Gravity.CENTER);
+            loc.setText(res.getLoc());
+
+            TextView dateText = new TextView(getContext());
+            dateText.setLayoutParams((layoutParams));
+            dateText.setGravity(Gravity.CENTER);
+            dateText.setText(res.getDate());
+
+            if (layout != null) {
+                layout.addView(loc);
+                layout.addView(dateText);
+                layout.addView(timeText);
+            }
+        }
+    }
+
+    private void getUpcomingEvents() {
+        userDB=getContext().getSharedPreferences("usersFile", Context.MODE_PRIVATE);
+        String userID=userDB.getInt("currentUser",1)+"";
+        String upcoming[]=userDB.getString(userID, "none").split(",");
+
+        for(int i=0;i<upcoming.length;i++){
+            if(i<=3){
+                continue;
+            }
+            else{
+                System.out.println("adding: "+upcoming[i]);
+                Reservation res= new Reservation(upcoming[i], getContext());
+                if(isUpcoming(res)){
+                    comingRes.add(res);
+                }
+            }
+        }
+
+
+
+    }
+
+    private boolean isUpcoming(Reservation res) {
+        return true;
+    }
+
+
 }
